6ffe381ba98f156b039c9a17bbd6a03ffb187aea
An <an.t.dotrong@gmail.com>
2025-06-01 15:19:27 +0700

    test base config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,9 @@ export default {
   plugins: [`~/plugins/lottie-vue-player.client.js`],
   ssr: true,           // Keep SSR enabled (still builds static HTML)
   target: 'static',    // Optional for Nuxt 3, still good for clarity
-  // router: {
-  //   base: '/pages' // 👈 Required for GitHub Pages
-  // },
+  router: {
+    base: '/pages' // 👈 Required for GitHub Pages
+  },
   app: {
     baseURL: '/pages'
   }
